c8bfb3a40ce0bfce4124d06b4b922e82ce6dc5b2
Jarmmo <jami.knaappila00@gmail.com>
2018-08-04 21:41:01 +0300

    added playermodels on spawn

--- gamemodes/placeholder_name/gamemode/init.lua
+++ gamemodes/placeholder_name/gamemode/init.lua
@@ -1,3 +1,33 @@
 AddCSLuaFile("cl_init.lua")
 AddCSLuaFile("sh_init.lua")
 include("sh_init.lua")
+
+local playermodels = 
+{
+	"models/player/group01/female_01.mdl",
+	"models/player/group01/female_02.mdl",
+	"models/player/group01/female_03.mdl",
+	"models/player/group01/female_04.mdl",
+	"models/player/group01/female_05.mdl",
+	"models/player/group01/female_06.mdl",
+	"models/player/group01/male_01.mdl",
+	"models/player/group01/male_02.mdl",
+	"models/player/group01/male_03.mdl",
+	"models/player/group01/male_04.mdl",
+	"models/player/group01/male_05.mdl",
+	"models/player/group01/male_06.mdl",
+	"models/player/group01/male_07.mdl",
+	"models/player/group01/male_08.mdl",
+	"models/player/group01/male_09.mdl"
+}
+
+function GM:PlayerInitialSpawn( ply )
+	ply.SetTeam(1)
+end
+
+function GM:PlayerSpawn(ply)
+	hook.Call( "PlayerLoadout", GAMEMODE, ply )
+	local col = team.GetColor(ply:Team())
+	ply:SetModel(table.Random(playermodels))
+	ply:SetPlayerColor(Vector(col.r/255,col.g/255,col.b/255))
+end
--- gamemodes/placeholder_name/gamemode/sh_init.lua
+++ gamemodes/placeholder_name/gamemode/sh_init.lua
@@ -2,5 +2,10 @@ GM.Name = "placeholder_name"
 GM.Author = "Jarmmo"
 
 function GM:Initialize()
-	-- Do stuff
+
+end
+
+function GM:CreateTeams()
+	team.SetUp( 0, "Spectator", Color(200, 200, 200))
+	team.SetUp( 1, "Player", Color(100, 255, 100))
 end
